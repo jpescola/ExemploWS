c6ccc09eed4600cabd3903b3024a57af0402d5b1
jpescola <jpescola@gmail.com>
2022-01-28 15:31:26 -0300

    Initial commit

---
+++ app/src/main/java/br/com/jpescola/exemplows/MainActivity.kt
@@ -0,0 +1,28 @@
+package br.com.jpescola.exemplows
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import okhttp3.*
+import java.io.IOException
+
+class MainActivity : AppCompatActivity() {
+    private val client = OkHttpClient()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        run("https://api.github.com/users/jpescola/repos")
+    }
+
+    private fun run(url: String) {
+        val request = Request.Builder()
+            .url(url)
+            .build()
+
+        client.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {}
+            override fun onResponse(call: Call, response: Response) = println(response.body()?.string())
+        })
+    }
+}
